c116421915a57967068c30248ed9897d973b8637
MiliAxe <miliaxe0@gmail.com>
2025-01-23 11:00:30 +0330

    add working jkb42 config without rgb and oled

---
+++ keyboards/jadookb/jkb42/rules.mk
@@ -0,0 +1,7 @@
+SERIAL_DRIVER = vendor
+RAW_ENABLE = yes
+QUANTUM_PAINTER_DRIVERS += st7735_spi
+CONSOLE_ENABLE = no
+# SRC += gfx/winxp/winxp.qgf.c
+# SRC += gfx/logo/logo.qgf.c
+# SRC += gfx/cat/cat.qgf.c
